ec6dd8e9a54838ebf62f79859c9a7bb67610ffe7
Shaoran <shaoran@sakuranohana.org>
2018-06-17 22:21:51 +0200

    adding gem with "bundle gem"

---
+++ lib/status-page.rb
@@ -0,0 +1,5 @@
+require "status-page/version"
+
+module StatusPage
+  # Your code goes here...
+end
---
+++ spec/status/page_spec.rb
@@ -0,0 +1,6 @@
+RSpec.describe StatusPage do
+  it "has a version number" do
+    expect(StatusPage::VERSION).not_to be nil
+  end
+
+end
