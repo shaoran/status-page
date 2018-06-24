d180a9d92ec6ef62d94b54d4f6b0e67b2c424c50
Shaoran <shaoran@sakuranohana.org>
2018-06-24 23:04:33 +0200

    adding new model "Pages" for storing the stats

--- lib/status-page/activerecord.rb
+++ lib/status-page/activerecord.rb
@@ -1,1 +1,11 @@
 require "status-page/activerecord/activerecord"
+
+dir = File.dirname(__FILE__)
+
+Dir[File.join(dir, "activerecord", "*.rb")].each do |fn|
+  fn = File.basename(fn, ".rb")
+
+  next if fn == "activerecord"
+
+  require "status-page/activerecord/#{fn}"
+end
